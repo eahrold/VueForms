b87459bcdc70281b61388c9b5726365ccc1c2c69
Eldon Ahrold <eahrold@bigclubdigital.com>
2018-05-03 22:07:21 -0500

    Fix import for case sensitive fs

--- src/prototypes/vfalert.js
+++ src/prototypes/vfalert.js
@@ -1,4 +1,4 @@
-import Vue from 'Vue'
+import Vue from 'vue'
 import _ from 'lodash'
 
 export const types = {
--- src/prototypes/vfconfig.js
+++ src/prototypes/vfconfig.js
@@ -1,4 +1,4 @@
-import Vue from 'Vue'
+import Vue from 'vue'
 import _ from 'lodash'
 import mimeTypes from '../data_sources/SupportedMimeTypes'
 
